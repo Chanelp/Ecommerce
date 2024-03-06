a4966630682c4c6a827559b23f78b4e2024323e6
chanelp <mariannissanchez27@gmail.com>
2024-03-05 23:11:52 -0400

    Componente Navbar y uso de NavLink de React Router

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,6 +6,7 @@ import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import './App.css'
+import NavBar from '../../Components/Navbar';
 
 const AppRoutes = () => {
   let routes = useRoutes([
@@ -23,6 +24,7 @@ const AppRoutes = () => {
 const App = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <AppRoutes />
     </BrowserRouter>
   )
