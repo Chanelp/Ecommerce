515b04824403f78e335e3bcc35580c37e9fcbb61
chanelp <mariannissanchez27@gmail.com>
2024-03-07 13:42:13 -0400

    Consumiendo la FakeStore API para pintar cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,14 @@
-const Card = () => {
+const Card = ({ data }) => {
     return(
         <article className='w-56 h-60 bg-white cursor-pointer rounded-lg'>
             <figure className='relative w-full h-4/5'>
-                <caption className='absolute bottom-0 left-0 bg-white/60 m-2 px-3 py-0.5 text-xs text-black font-semibold rounded-lg'>Electronics</caption>
-                <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='Headphones'/>
+                <figcaption className='absolute bottom-0 left-0 bg-white/60 m-2 px-3 py-0.5 text-xs text-black font-semibold rounded-lg'> {data?.category?.name} </figcaption>
+                <img className='w-full h-full object-cover rounded-lg' src={data?.images[1]} alt={data?.description} loading="lazy" />
                 <button className="flex justify-center w-6 h-6 items-center absolute top-0 right-0 bg-white m-2 rounded-full font-bold">+</button>
             </figure>
             <p className="flex justify-between">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-medium">$300</span>
+                <span className="text-sm font-light"> { data?.title } </span>
+                <span className="text-lg font-medium">${ data?.price } </span>
             </p>
         </article>
     );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react";
 import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const data = await fetch('https://api.escuelajs.co/api/v1/products')
+        const jsonData = await data.json()
+        setItems(jsonData)
+      } 
+      catch (error) {
+        console.log(error)
+      }
+    }
+    getProducts()
+  }, [])
+
     return (
       <>
         Home
-        <Card />
+        <section className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map(item => (
+              <Card key={item.id} data={item} />
+              ))
+          }
+        </section>
       </>
     )
   }
