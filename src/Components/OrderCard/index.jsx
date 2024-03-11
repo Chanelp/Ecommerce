f7bf7d22682524766023d4cdcfbe2956514492f1
chanelp <mariannissanchez27@gmail.com>
2024-03-11 17:31:20 -0400

    Componente OrderCard

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,11 +1,12 @@
 import { TrashIcon } from '@heroicons/react/24/solid';
+import './style.css'
 
 const OrderCard = props => {
 
     const { title, price } = props;
 
     return(
-        <div className="flex justify-between items-center w-full mb-3">
+        <div className="order flex justify-between items-center w-full mb-3 rounded-lg p-2">
             <div className="flex items-center gap-2">
                 <figure className='w-20 h-20'>
                     <img className='w-full h-full object-cover rounded-lg' src="https://i.imgur.com/cHddUCu.jpeg" alt={title} />
@@ -14,7 +15,7 @@ const OrderCard = props => {
             </div>
             <div className="flex items-center gap-2">
                 <p className='font-medium text-base'>${price}</p>
-                <button className='w-6 h-6 text-red-600'>
+                <button className='w-6 h-6 text-gray-300 hover:text-red-600'>
                     <TrashIcon></TrashIcon>
                 </button>
             </div>
