81b32c8bb0251acb1bcbcec7bc57b41c3579e207
chanelp <mariannissanchez27@gmail.com>
2024-03-11 19:02:08 -0400

    Evitando productos duplicados en el carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,5 @@
 import { PlusIcon } from '@heroicons/react/24/solid';
+import { CheckIcon } from '@heroicons/react/24/solid';
 import { useShoppingCartProvider } from '../../Context';
 
 const Card = ({ data }) => {
@@ -18,16 +19,39 @@ const Card = ({ data }) => {
         closeProductDetail();
     }
 
-    return(
-        <article className='w-56 h-60 bg-white cursor-pointer rounded-lg' onClick={() => showProduct(data)}>
-            <figure className='relative w-full h-4/5'>
-                <figcaption className='absolute bottom-0 left-0 bg-white/60 m-2 px-3 py-0.5 text-xs text-black font-semibold rounded-lg hover:bg-white'> {data?.category?.name} </figcaption>
-                <img className='w-full h-full object-cover rounded-2xl' src={data?.images[0]} alt={data?.description} loading='lazy' />
+    const renderIcon = (id) => {
+        const isInCart = cartProducts.filter(product => product.id === id).length > 0;
+
+        if(isInCart) {
+            return (
+                <button className='flex justify-center w-6 h-6 items-center absolute top-0 right-0 bg-black m-2 rounded-full font-bold text-white'>
+                    <CheckIcon className='w-4 h-4'></CheckIcon>
+                </button>
+            );
+        } 
+        else {
+            return(
                 <button className='flex justify-center w-6 h-6 items-center absolute top-0 right-0 bg-white m-2 rounded-full font-bold hover:bg-red-800 hover:text-white'
                     onClick={(event) => addProductsToCart(event, data)}
                 >
                     <PlusIcon className='w-4 h-4'></PlusIcon>
                 </button>
+            );
+        }
+    }
+
+    return(
+        <article className='w-56 h-60 bg-white cursor-pointer rounded-lg' onClick={() => showProduct(data)}>
+            <figure className='relative w-full h-4/5'>
+                <figcaption className='absolute bottom-0 left-0 bg-white/60 m-2 px-3 py-0.5 text-xs text-black font-semibold rounded-lg hover:bg-white'> 
+                    {data?.category?.name} 
+                </figcaption>
+                <img className='w-full h-full object-cover rounded-2xl' 
+                    src={data?.images[0]} 
+                    alt={data?.description} 
+                    loading='lazy' 
+                />
+                {renderIcon(data.id)}
             </figure>
             <p className='flex justify-between mt-2'>
                 <span className='text-sm font-light'> { data?.title } </span>
