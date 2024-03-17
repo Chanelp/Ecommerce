4cddfe4ecaa3f66dd607b63fa8d32dea6e635a58
chanelp <mariannissanchez27@gmail.com>
2024-03-17 12:13:59 -0400

    Estilos: myOrders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,12 +1,14 @@
+import { ShoppingCartIcon, CurrencyDollarIcon, CalendarDaysIcon } from '@heroicons/react/24/outline';
+
 const OrdersCard = (props) => {
     const { date, totalPrice, quantityProducts } = props;
 
     return(
-        <div className="flex justify-between items-center mb-3 p-6 border border-black rounded-lg">
-            <p className="flex justify-between w-80">
-                <span>{date}</span>
-                <span>{quantityProducts}</span>
-                <span>{totalPrice}</span>
+        <div className="flex justify-between items-center mb-3 p-6 border-2 border-zinc-300 rounded-lg">
+            <p className="flex justify-between w-96">
+                <span className='flex justify-center items-center gap-2'><ShoppingCartIcon width="20px" height="20px" /> {quantityProducts} products</span>
+                <span className='flex justify-center items-center gap-2'><CurrencyDollarIcon width="20px" height="20px" /> ${totalPrice}</span>
+                <span className='flex justify-center items-center gap-2'><CalendarDaysIcon width="20px" height="20px" />{date}</span>
             </p>
         </div>
     );
